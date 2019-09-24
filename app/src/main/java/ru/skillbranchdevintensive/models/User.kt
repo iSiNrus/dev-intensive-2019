e554e6cfeb2d025def89f738db5606bfadadf7a4
iSiNrus <magnit_322@rambler.ru>
2019-09-24 13:44:54 +0400

    User task: user class + util

---
+++ app/src/main/java/ru/skillbranchdevintensive/models/User.kt
@@ -0,0 +1,50 @@
+package ru.skillbranchdevintensive.models
+
+import ru.skillbranchdevintensive.utils.Utils
+import java.util.*
+
+class User (
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = null,
+    var isOnline : Boolean = false
+){
+    constructor( id:String, firstName: String?, lastName: String?) : this(id, firstName, lastName, null)
+
+    constructor(id: String): this(id, "John", "Conor $id")
+
+    init {
+        printMe()
+        println( if (firstName=="John" && lastName=="Cena")
+                    "tu tu ru tuuuu!!!"
+                else "A'm Alive")
+    }
+
+    fun printMe():Unit{
+        println("""
+    id: $id
+    firstName: $firstName
+    lastName: $lastName
+    avatar: $avatar
+    rating: $rating
+    respect: $respect
+    lastVisit: $lastVisit
+    isOnline: $isOnline
+        """.trimIndent())
+    }
+
+    companion object Factory{
+        private var lastId : Int = -1
+        fun makeUser(fullName: String?):User{
+            lastId++
+
+            val (f_name, l_name) = Utils.parseFullName(fullName)
+
+            return User(id = "$lastId", firstName = "$f_name", lastName = "$l_name")
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranchdevintensive/utils/Utils.kt
@@ -0,0 +1,12 @@
+package ru.skillbranchdevintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?): Pair<String?, String?>{
+        val parts : List<String>? = fullName?.split(" ")
+        var f_name = parts?.getOrNull(0)
+        var l_name = parts?.getOrNull(1)
+        if(f_name=="") f_name=null
+        if(l_name=="") l_name=null
+        return Pair(f_name, l_name)
+    }
+}
